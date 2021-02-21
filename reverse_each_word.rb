d63f5126cf919beab96a8d8efae90cf802457929
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-21 21:08:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
-def reverse_each_word(sentence)
- array << sentence.split
- array.each {|word| word.reverse}
+def reverse_each_word(string)
+  array = string.split(" ")
+
+  new_array = array.collect do |word|
+  word.reverse
+  end
+  new_array.join(" ")
 end
